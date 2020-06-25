6921397e8f83e7ea133e5555eec71138671aceec
talgat55 <lightx_design@yahoo.com>
2020-06-26 02:19:30 +0600

    add migrations

---
+++ database/migrations/2020_06_25_201256_create_groups_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('todo_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('groups', function($table) {
+            $table->foreign('todo_id')
+                ->references('id')->on('todo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
