2fabec8e8727af649ac3ddca7a1df0392e13523b
talgat55 <lightx_design@yahoo.com>
2020-06-29 00:46:59 +0600

    add first tamplate vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+window.Vue = require('vue');
+import App from './components/App';
+import vuetify from './vuetify';
+const app = new Vue({
+    vuetify,
+    el: '#app',
+    render: h => h(App),
+});
---
+++ resources/js/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-app>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <v-form
+                        ref="form"
+                    >
+                        <v-text-field
+                            v-model="group"
+                            label="Group"
+                            required
+                        ></v-text-field>
+                        <div class="my-2">
+                            <v-btn color="primary">Создать</v-btn>
+                        </div>
+                    </v-form>
+                </div>
+            </div>
+        </div>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        name: "App",
+        data: () => ({
+            valid: true,
+            group: '',
+        })
+    }
+</script>
